5728a32660715367505b6debf9dba7d2bafb98ab
mcapuccini <marco.capuccini@farmbio.uu.se>
2020-04-22 15:28:52 +0200

    remove superflous arg

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -7,6 +7,5 @@ docker pull $REPO_NAME || true # avoid fail if repo not pushed yet
 docker build \
     -t $REPO_NAME \
     -f .devcontainer/Dockerfile \
-    --build-arg BUILDKIT_INLINE_CACHE=1 \
     --cache-from $REPO_NAME \
     .
