c899b38e57384c5fdaaa40ed6ac41e5a0f8e945f
mcapuccini <marco.capuccini@riken.jp>
2020-06-23 11:14:29 +0000

    refactory

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -6,6 +6,5 @@ REPO_NAME=mcapuccini/gem5-sandbox
 docker pull $REPO_NAME || true # avoid fail if repo not pushed yet
 docker build \
     -t $REPO_NAME \
-    -f .build/Dockerfile \
     --cache-from $REPO_NAME \
     .
